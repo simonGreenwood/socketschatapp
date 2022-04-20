087966caf44d2493e552b7641401f3cc68ac659b
simonGreenwood <simonrobertgreenwood@gmail.com>
2022-04-20 21:31:07 +0100

    added tailwind

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,15 +4,13 @@ import styles from '../styles/Home.module.css'
 import {useRouter} from 'next/router'
 export default function Home() {
   const router = useRouter()
-  const { pid } = router.query
+  const handleJoinClicked = () => {
+    router.push('/room/doge')
+  }
+  const handleCreateClicked = () => {}
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Chat App</title>
-      </Head>
-      <Link href={`/room/420`}>
-        <a>this page!</a>
-      </Link>
+    <div className=''>
+      <h1 className='text-3xl blue'>Chatapp</h1>
     </div>
   )
 }
